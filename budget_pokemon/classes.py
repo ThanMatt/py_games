bb02bb33b2229d6a038d61f7106dd976a73d4f9e
llquillo <loisjanequillo@gmail.com>
2022-07-23 11:54:16 +0800

    budget-pokemon: added class functions and modified init

--- budget_pokemon/classes.py
+++ budget_pokemon/classes.py
@@ -2,13 +2,13 @@
 import os, time
 
 class Pokemon:
-	def __init__(self, name, hp, types, weaknesses, strengths, moves, speed):
+	def __init__(self, name, hp, weaknesses, strengths, speed):
 		self.name = name
 		self.hp = hp
-		self.types = types
+		self.types = []
 		self.weaknesses = weaknesses
 		self.strengths = strengths
-		self.moves = moves
+		self.moves = []
 		self.speed = speed
 
 	def get_name(self):
@@ -20,17 +20,32 @@ class Pokemon:
 	def get_speed(self):
 		return self.speed
 
+	def set_moves(self, moves):
+		self.moves = moves
+
+	def set_types(self, types):
+		self.types = types
+
 class Move:
 	PHYSICAL = 'Physical'
 	SPECIAL = 'Special'
 	STATUS = 'Status'
 
-	def __init__(self, name, damage, accuracy, pp, move_type, category):
+	def __init__(self, name, damage, accuracy, pp, move_type, status_ailment):
 		self.name = name
 		self.damage = damage
 		self.pp = pp
 		self.move_type = move_type
-		self.category = category
+		self.category = ''
+		self.status_ailment = status_ailment
+
+	def map_status(self, category):
+		map_to_constant = {
+			"Physical": self.PHYSICAL,
+			"Special": self.SPECIAL,
+			"Status": self.STATUS
+		}
+		return map_to_constant[category]
 
 
 class Type:
@@ -39,9 +54,20 @@ class Type:
 		self.weakness = []
 		self.strength = []
 
+	def map_name(self, name):
+		return self.name.lower() == name.lower()
 
-	
+	def set_weakness(self, weakness):
+		self.weakness = weakness
 
+	def set_strength(self, strength):
+		self.strength = strength
+
+	def get_strength(self):
+		return self.strength
+
+	def get_weakness(self):
+		return self.strength
 
 
 
--- budget_pokemon/init.py
+++ budget_pokemon/init.py
@@ -4,43 +4,84 @@ import classes
 import json
 
 script_dir = os.path.dirname(__file__)
-file_path = os.path.join(script_dir, 'json/monsters.json')
-monsters = open(file_path, 'r')
-data = json.load(monsters)
 
+pokemon_data = {}
+types_data = {}
+
+types = []
+pokemons = []
+
+def init_json(path):
+	# :: Initializes json files
+
+	file_path = os.path.join(script_dir, path)
+	file = open(file_path, 'r')
+	data = json.load(file)
+	return data
+
+def init_moves(moves):
+	# :: Creates class for each Pokemon move
+	moves_arr = []
+	for move in moves:
+		move_obj = classes.Move(
+			name=move["name"],
+			damage=move["damage"],
+			pp=move["pp"],
+			move_type=move["move_type"],
+			accuracy=move["accuracy"],
+			status_ailment=move["status_ailment"]
+		)
+		move_obj.category=move_obj.map_status(move["category"])
+		moves_arr.append(move_obj)
+
+	return moves_arr
+
+def init_types_for_pokemon(types, const_types):
+	# :: Maps the initialized Types array to the Pokemon types
+
+	types_arr = []
+	for typ in types:
+		for i in const_types:
+			if(i.map_name(typ)):
+				types_arr.append(i)
 
 def init_game():
-	# init electric type
-	electric_type = classes.Type('Electric')
+	# :: Initialize global variables
+	global pokemon_data, types_data
+	global types, pokemon
 
-	# :: Pokemons to choose
-	pokemons = []
+	# :: Initialize json files
+	pokemon_data = init_json('json/monsters.json')
+	types_data = init_json('json/types.json')
 
-	# init move 1
-	pikachu_move_1 = classes.Move('Thunder Shock', 40, 100, 30, electric_type, classes.Move.SPECIAL)
+	# :: Initialize main Types classes
+	for type_data in types_data['types']:
+		name = type_data+'_obj'
+		name = classes.Type(
+			name=type_data
+		)
+		print(name)
+		types.append(name)
+
 
 	# :: Initialize pokemons
-	for pokemon in data['pokemons']:
-		pokemons.append(
-			classes.Pokemon(
-				name=pokemon["name"],
-				hp=pokemon["hp"],
-				types=pokemon["types"],
-				weaknesses=pokemon["weaknesses"],
-				strengths=pokemon["strengths"],
-				moves=pokemon["moves"],
-				speed=pokemon["speed"]
-			)
+	for pokemon in pokemon_data['pokemons']:
+		pokemon_obj = classes.Pokemon(
+			name=pokemon["name"],
+			hp=pokemon["hp"],
+			weaknesses=pokemon["weaknesses"],
+			strengths=pokemon["strengths"],
+			speed=pokemon["speed"]
 		)
+		pokemon_obj.set_types(init_types_for_pokemon(pokemon["types"], types))
+		pokemon_obj.set_moves(init_moves(pokemon["moves"]))
+		pokemons.append(pokemon_obj)
+		
 
 	for pokemon in pokemons:
 		print(pokemon.get_name(), pokemon.get_speed())
-	# init pokemon 1
-	# pikachu = classes.Pokemon(
-	# 		name='Pikachu',
-	# 		hp=100,
-	# 		types=['electric',],
-	# 		moves=[pikachu_move_1.category,]
-	# 	)
+		print(pokemon.types)
+		# for move in pokemon.moves:
+		# 	print(move.category)
 
-init_game()
+
